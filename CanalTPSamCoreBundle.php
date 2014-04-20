f846ed87105100016fc88371807613135edca8a8
dql <david.quintanel@canaltp.fr>
2014-04-20 19:18:06 +0200

    added RoleListener in order to canonicalize thr role's name

--- CanalTPSamCoreBundle.php
+++ CanalTPSamCoreBundle.php
@@ -2,8 +2,16 @@
 
 namespace CanalTP\SamCoreBundle;
 
+use CanalTP\SamCoreBundle\DependencyInjection\Compiler\DoctrineEntityListenerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CanalTPSamCoreBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new DoctrineEntityListenerPass());
+    }
 }
--- Entity/Role.php
+++ Entity/Role.php
@@ -73,7 +73,6 @@ class Role
     public function setName($name)
     {
         $this->name = $name;
-        $this->setCanonicalName($this->canonicalize($name));
 
         return $this;
     }
@@ -230,35 +229,5 @@ class Role
         while ($i-- > 0) {
             $this->permissions[] = '';
         }
-    }
-
-    // @todo better in an event
-    private function canonicalize($role)
-    {
-        return strtoupper('ROLE_' . $this->slugify($role, '_'));
-    }
-
-    public function slugify($text, $separator = '-')
-    {
-        // replace non letter or digits by -
-        $text = preg_replace('~[^\\pL\d]+~u', $separator, $text);
-
-        // trim
-        $text = trim($text, $separator);
-
-        // transliterate
-        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-
-        // lowercase
-        $text = strtolower($text);
-
-        // remove unwanted characters
-        $text = preg_replace('~[^-\w]+~', '', $text);
-
-        if (empty($text)) {
-            return 'n-a';
-        }
-
-        return $text;
     }
 }
