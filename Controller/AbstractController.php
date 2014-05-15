c079abc8a5c8d9b1df91278a52d354bcd3b3951d
kzi <kevin.ziemianski@canaltp.fr>
2014-05-15 18:10:15 +0200

    Add AbstractController

---
+++ Controller/AbstractController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CanalTP\SamCoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+abstract class AbstractController extends Controller
+{
+    protected function checkPermission($object, $permission)
+    {
+        if (false === $this->get('security.context')->isGranted($permission, $object)) {
+            throw new AccessDeniedException();
+        }
+    }
+    
+    protected function isGranted($permission)
+    {
+        return $this->get('security.context')->isGranted($permission);
+    }
+    
+    protected function isAllow($permission)
+    {
+        if ($this->isGranted($permission) === false) {
+            throw new AccessDeniedException($this->get('translator')->trans('forbidden'));
+        }
+    }
+}
