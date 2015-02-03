8c4f9fa11866f76253b1dfa8a9f21e7ae2cafe31
kzi <kevin.ziemianski@canaltp.fr>
2015-02-03 14:42:05 +0100

    Add '

--- Migrations/pdo_pgsql/Version009.php
+++ Migrations/pdo_pgsql/Version009.php
@@ -16,20 +16,20 @@ class Version009 extends AbstractMigration
 
     public function up(Schema $schema)
     {
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPMttBundle WHERE app_canonnical_name='mtt'");
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPMatrixBundle WHERE app_canonnical_name='matrix'");
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPNmpAdminBundle WHERE app_canonnical_name='nmpadmin'");
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPRealTimeBundle WHERE app_canonnical_name='realtime'");
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPNmmPortalBridgeBundle WHERE app_canonnical_name='samcore'");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPMttBundle' WHERE app_canonnical_name='mtt'");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPMatrixBundle' WHERE app_canonnical_name='matrix'");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPNmpAdminBundle' WHERE app_canonnical_name='nmpadmin'");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPRealTimeBundle' WHERE app_canonnical_name='realtime'");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPNmmPortalBridgeBundle' WHERE app_canonnical_name='samcore'");
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPMttBundle WHERE app_canonnical_name=''");
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPMatrixBundle WHERE app_canonnical_name=''");
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPNmpAdminBundle WHERE app_canonnical_name=''");
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPRealTimeBundle WHERE app_canonnical_name=''");
-        $this->addSql("UPDATE tr_application_app SET app_bundle_name = CanalTPNmmPortalBridgeBundle WHERE app_canonnical_name=''");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPMttBundle' WHERE app_canonnical_name=''");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPMatrixBundle' WHERE app_canonnical_name=''");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPNmpAdminBundle' WHERE app_canonnical_name=''");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPRealTimeBundle' WHERE app_canonnical_name=''");
+        $this->addSql("UPDATE tr_application_app SET app_bundle_name = 'CanalTPNmmPortalBridgeBundle' WHERE app_canonnical_name=''");
     }
 }
 
