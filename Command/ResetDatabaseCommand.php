557454f7c53a6c4a1215e856c94de97396de73f2
Rémy Abi-Khalil <remyabi@gmail.com>
2015-02-23 14:43:51 +0100

    Refs #METH-378 ~ Command to Load fixture updated.

--- Command/ResetDatabaseCommand.php
+++ Command/ResetDatabaseCommand.php
@@ -42,26 +42,26 @@ EOT
     {
         $this->env = $this->getContainer()->get('kernel')->getEnvironment();
         $this->console = $this->getApplication();
+
+        preg_match_all(
+            "|(?P<namespace>[^,]*)\\\(?P<application>[^\\\]*)BridgeBundle|U",
+            implode(',', $this->getContainer()->getParameter('kernel.bundles')),
+            $bridges,
+            PREG_SET_ORDER
+        );
+
         $this->console->setAutoExit(false);
         $this->console->setCatchExceptions(false);
-
         // Drop the database
         $this->runCommand('doctrine:database:drop', array('--force'  => true), $output);
-
         // Create the database
         $this->runCommand('doctrine:database:create', array(), $output);
-
         // Create tables for each aplications
-        $this->runCommand('claroline:migration:upgrade', array('bundle' => 'CanalTPSamCoreBundle', '--target' => 'farthest'), $output);
-        $this->runCommand('claroline:migration:upgrade', array('bundle' => 'CanalTPNmmPortalBundle', '--target' => 'farthest'), $output);
-        $this->runCommand('claroline:migration:upgrade', array('bundle' => 'CanalTPMttBundle', '--target' => 'farthest'), $output);
-        $this->runCommand('claroline:migration:upgrade', array('bundle' => 'CanalTPNmpAdminBundle', '--target' => 'farthest'), $output);
-        $this->runCommand('claroline:migration:upgrade', array('bundle' => 'CanalTPRealTimeBundle', '--target' => 'farthest'), $output);
-        $this->runCommand('claroline:migration:upgrade', array('bundle' => 'CanalTPMatrixBundle', '--target' => 'farthest'), $output);
-
+        foreach ($bridges as $bridge) {
+            $this->runCommand('claroline:migration:upgrade', array('bundle' => $bridge['namespace'] . $bridge['application'] . "Bundle", '--target' => 'farthest'), $output);
+        }
         // Fixtures
         $this->runCommand('doctrine:fixtures:load', array('--append'  => true), $output);
-        $this->runCommand('claroline:migration:upgrade', array('bundle' => 'CanalTPMetricsDashboardBundle', '--target' => 'farthest'), $output);
     }
 
     private function runCommand($command, $arguments, OutputInterface $output)
